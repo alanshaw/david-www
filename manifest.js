f3040e54fb37a4f104067de78598ff6f626243a2
Alan Shaw <alan@freestyle-developments.co.uk>
2013-01-27 23:28:41 +0000

    Cache manifests for 1 hour

--- manifest.js
+++ manifest.js
@@ -1,8 +1,26 @@
 var request = require('request');
+var moment = require('moment');
 
 var exports = {};
 
+function Manifest(data) {
+	this.data = data;
+	this.expires = moment().add(Manifest.TTL);
+}
+
+Manifest.TTL = moment.duration({hours: 1});
+
+var manifests = {};
+
 exports.getManifest = function(url, callback) {
+	
+	var manifest = manifests[url];
+	
+	if(manifest && manifest.expires > new Date()) {
+		console.log('Using cached manifest', manifest.data.name, manifest.data.version);
+		callback(null, JSON.parse(JSON.stringify(manifest.data)));
+		return;
+	}
 	
 	request(url, function(err, response, body) {
 		
@@ -18,7 +36,11 @@ exports.getManifest = function(url, callback) {
 				
 				console.log('Got manifest', data.name, data.version);
 				
-				callback(null, data);
+				manifest = new Manifest(data);
+				
+				manifests[url] = manifest;
+				
+				callback(null, manifest.data);
 			}
 			
 		} else if(!err) {
@@ -34,6 +56,15 @@ exports.getManifest = function(url, callback) {
 
 exports.getGithubManifestUrl = function(username, repo) {
 	return 'https://raw.github.com/' + username + '/' + repo + '/master/package.json';
+};
+
+/**
+ * Set the TTL for cached manifests.
+ * 
+ * @param {moment.duration} duration Time period the manifests will be cahced for, expressed as a moment.duration.
+ */
+exports.setCacheDuration = function(duration) {
+	Manifest.TTL = duration;
 };
 
 module.exports = exports;
