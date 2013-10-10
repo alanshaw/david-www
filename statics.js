46c8c0d8243175369cf76e482af46346f6507b46
Alan Shaw <alan@freestyle-developments.co.uk>
2013-10-10 19:46:32 +0100

    Refs #61 - Increase expires time to 7 days (for most versioned assets)

--- statics.js
+++ statics.js
@@ -2,15 +2,15 @@ var express = require('express');
 
 module.exports.init = function(app) {
 
-	app.use('/js', express.static(__dirname + '/dist/js', {maxAge: 86400000}));
-	app.use('/css', express.static(__dirname + '/dist/css', {maxAge: 86400000}));
-	app.use('/img', express.static(__dirname + '/dist/img', {maxAge: 86400000}));
-	app.use('/font', express.static(__dirname + '/dist/font', {maxAge: 86400000}));
-	app.use('/inc', express.static(__dirname + '/dist/inc', {maxAge: 86400000}));
+	app.use('/js', express.static(__dirname + '/dist/js', {maxAge: 86400000 * 7}));
+	app.use('/css', express.static(__dirname + '/dist/css', {maxAge: 86400000 * 7}));
+	app.use('/img', express.static(__dirname + '/dist/img', {maxAge: 86400000 * 7}));
+	app.use('/font', express.static(__dirname + '/dist/font', {maxAge: 86400000 * 7}));
+	app.use('/inc', express.static(__dirname + '/dist/inc', {maxAge: 3600000}));
 
-	app.use(express.favicon(__dirname + '/dist/favicon.ico', {maxAge: 86400000}));
+	app.use(express.favicon(__dirname + '/dist/favicon.ico', {maxAge: 86400000 * 7}));
 
 	app.get('/apple-touch-icon-precomposed.png', function(req, res) {
-		res.sendfile(__dirname + '/dist' + req.url, {maxAge: 86400000});
+		res.sendfile(__dirname + '/dist' + req.url, {maxAge: 86400000 * 7});
 	});
 };
