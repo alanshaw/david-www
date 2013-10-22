b0d31962fc495128093e3a827ec725ecea567dfa
XhmikosR <xhmikosr@users.sourceforge.net>
2013-10-22 20:43:20 +0300

    Use variables for the expires date.

--- statics.js
+++ statics.js
@@ -1,15 +1,18 @@
 var express = require('express');
 
+var oneDay = 86400000;  // milliseconds: 60 * 60 * 24 * 1000
+var oneWeek = oneDay * 7;
+
 module.exports.init = function(app) {
 
-	app.use('/js', express.static(__dirname + '/dist/js', {maxAge: 86400000 * 7}));
-	app.use('/css', express.static(__dirname + '/dist/css', {maxAge: 86400000 * 7}));
-	app.use('/img', express.static(__dirname + '/dist/img', {maxAge: 86400000 * 7}));
-	app.use('/font', express.static(__dirname + '/dist/font', {maxAge: 86400000 * 7}));
+	app.use('/css', express.static(__dirname + '/dist/css', { maxAge: oneWeek }));
+	app.use('/font', express.static(__dirname + '/dist/font', { maxAge: oneWeek }));
+	app.use('/img', express.static(__dirname + '/dist/img', { maxAge: oneWeek }));
+	app.use('/js', express.static(__dirname + '/dist/js', { maxAge: oneWeek }));
 
-	app.use(express.favicon(__dirname + '/dist/favicon.ico', {maxAge: 86400000 * 7}));
+	app.use(express.favicon(__dirname + '/dist/favicon.ico', { maxAge: oneWeek }));
 
 	app.get('/apple-touch-icon-precomposed.png', function(req, res) {
-		res.sendfile(__dirname + '/dist' + req.url, {maxAge: 86400000 * 7});
+		res.sendfile(__dirname + '/dist' + req.url, { maxAge: oneWeek });
 	});
 };
