8ee77a2c78fec2908e2c4148c69bf6eed96daa17
XhmikosR <xhmikosr@gmail.com>
2015-03-04 00:58:09 +0200

    Add load-grunt-tasks.

--- Gruntfile.js
+++ Gruntfile.js
@@ -111,17 +111,8 @@ module.exports = function(grunt) {
     }
   })
 
-  // Load the grunt plugins
-  grunt.loadNpmTasks("grunt-browserify")
-  grunt.loadNpmTasks("grunt-contrib-clean")
-  grunt.loadNpmTasks("grunt-contrib-copy")
-  grunt.loadNpmTasks("grunt-contrib-cssmin")
-  grunt.loadNpmTasks("grunt-contrib-jshint")
-  grunt.loadNpmTasks("grunt-contrib-less")
-  grunt.loadNpmTasks("grunt-contrib-nodeunit")
-  grunt.loadNpmTasks("grunt-contrib-uglify")
-  grunt.loadNpmTasks("grunt-contrib-watch")
-  grunt.loadNpmTasks("grunt-include-replace")
+  // Load any grunt plugins found in package.json.
+  require("load-grunt-tasks")(grunt, {scope: "devDependencies"});
 
   grunt.registerTask("base", ["jshint", "nodeunit", "copy", "includereplace", "less:compile", "browserify"])
   grunt.registerTask("min", ["cssmin", "uglify"])
