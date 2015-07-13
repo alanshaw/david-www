b3e432a7d0f29b3393529ff3ba1b7b0e3701ecfb
James Messinger <james.messinger@gmail.com>
2015-07-13 10:46:10 -0500

    Respond to OPTIONS requests immediately

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,5 +1,11 @@
 module.exports = function(req, res, next) {
-    res.set("Access-Control-Allow-Origin", "*");
-    res.set("Access-Control-Allow-Methods", "GET");
+  res.set("Access-Control-Allow-Origin", "*");
+  res.set("Access-Control-Allow-Methods", "GET");
+
+  if (req.method === 'OPTIONS') {
+    res.send();
+  }
+  else {
     next();
+  }
 };
