bb0696f0ea6a6ea91ded97976a81c66d55a30e7c
Alan Shaw <alan@tableflip.io>
2015-09-09 21:54:56 +0100

    Cache manifests in leveldb

--- middleware/session.js
+++ middleware/session.js
@@ -1,4 +1,4 @@
-var config = require("config")
+var db = require("../lib/db")
 var levelSession = require("level-session")
 
-module.exports = levelSession(config.db.path)
+module.exports = levelSession({db: db})
