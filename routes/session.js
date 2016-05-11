7f5b451d581f50ae97c4835c0457135a3aadf036
Alan Shaw <alan@tableflip.io>
2016-05-11 23:36:54 +0100

    Render requires string arg

--- routes/session.js
+++ routes/session.js
@@ -2,11 +2,11 @@ module.exports.oauthCallback = (app, auth) => {
   app.get('/auth/callback', (req, res) => {
     req.session.get('session/csrf-token', (err, csrfToken) => {
       if (err || csrfToken !== req.query.state || !req.query.code) {
-        return res.status(401).render(401, err)
+        return res.status(401).render('401', err)
       }
 
       auth.requestAccessToken(req.query.code, (err, data) => {
-        if (err) return res.status(401).render(401, err)
+        if (err) return res.status(401).render('401', err)
 
         req.session.set('session/access-token', data.access_token, () => {
           req.session.set('session/user', data.user, () => {
