2eae5193828790053ec23f41ec8ef71ba71e9e05
Alan Shaw <alan@tableflip.io>
2016-05-24 08:34:06 +0100

    Add analytics component

---
+++ src/ui/components/analytics.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import Helmet from 'react-helmet'
+import Qs from 'querystring'
+
+const Analytics = React.createClass({
+  propTypes: {
+    routing: React.PropTypes.object.isRequired
+  },
+
+  componentDidMount () {
+    window.GoogleAnalyticsObject = 'ga'
+    window.ga = window.ga || function () {
+      (window.ga.q = window.ga.q || []).push(arguments)
+    }
+    window.ga.l = 1 * new Date()
+    window.ga('create', 'UA-38876227-1')
+    window.ga('send', 'pageview')
+  },
+
+  componentWillReceiveProps (props) {
+    const currUrl = this.buildUrl(this.props.routing.locationBeforeTransitions)
+    const nextUrl = this.buildUrl(props.routing.locationBeforeTransitions)
+
+    if (currUrl != nextUrl) {
+      window.ga('send', 'pageview')
+    }
+  },
+
+  buildUrl (location) {
+    let qs = Qs.stringify(location)
+    qs = qs ? `?${qs}` : ''
+    return `${location.pathname}${qs}${location.search}`
+  },
+
+  shouldComponentUpdate () {
+    return !(window && window.ga)
+  },
+
+  render () {
+    return <Helmet script={[{ src: '//www.google-analytics.com/analytics.js' }]} />
+  }
+})
+
+const mapStateToProps = ({ routing }) => ({ routing })
+
+export default connect(mapStateToProps)(Analytics)
--- src/ui/components/layout.jsx
+++ src/ui/components/layout.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './header.jsx'
 import Footer from './footer.jsx'
+import Analytics from './analytics.jsx'
 
 export default React.createClass({
   render () {
     return (
       <div>
+        <Analytics />
         <Header />
         <main id='main' className='clearfix' role='main'>
           {this.props.children}
