7898e199996a855643a0c9b35ac349fd135af64d
Alan Shaw <alan@tableflip.io>
2017-02-08 09:21:49 +0000

    More logs

--- src/lib/queue/worker.js
+++ src/lib/queue/worker.js
@@ -3,9 +3,19 @@ import Async from 'async'
 export default ({ manifest, brains, cache }) => {
   return ({ user, repo, opts }, cb) => {
     Async.waterfall([
-      (cb) => manifest.getManifest(user, repo, opts, cb),
+      (cb) => {
+        manifest.getManifest(user, repo, opts, (err, manifest) => {
+          if (err) return cb(err)
+          console.log(`Got manifest for ${user}/${repo}`)
+          cb(null, manifest)
+        })
+      },
       (manifest, cb) => {
-        brains.getInfo(manifest, opts, (err, info) => cb(err, manifest, info))
+        brains.getInfo(manifest, opts, (err, info) => {
+          if (err) return cb(err)
+          console.log(`Got info for ${user}/${repo}`)
+          cb(err, manifest, info)
+        })
       }
     ], (err, manifest, info) => {
       if (err) return cb(err)
